e7dd98614727fc08a4c5219ae5251dc4ab688b89
Michael Essex <123messex@gmail.com>
2021-05-18 10:25:18 -0500

    Initial Commit

---
+++ Api Calling/Api_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_CallingApp.swift
+//  Api Calling
+//
+//  Created by Student on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Api_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
